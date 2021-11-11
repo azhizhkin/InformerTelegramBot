bbc46f70e7cbd85cd15ba5a266c72ece05f291b5
AndreyZhizhkin <azhizhkin@gmail.com>
2021-11-11 20:40:16 +0200

    refact figconfigreader

--- internal/infrastructure/figconfigreader.go
+++ internal/infrastructure/figconfigreader.go
@@ -1,33 +1,18 @@
 package infrastructure
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/azhizhkin/informertelegrambot/internal/appconfig"
-	"github.com/jinzhu/copier"
 
 	"github.com/kkyr/fig"
 )
 
-type FigConfigReader struct {
-	Token  string
-	SQLite struct {
-		File string
-	}
-	GRPC struct {
-		Port string
-	}
-}
+type FigConfigReader appconfig.AppConfig
 
 func (figConfig FigConfigReader) GetAppConfig() *appconfig.AppConfig {
 	if err := fig.Load(&figConfig); err != nil {
 		log.Fatalf("Error reading config: %v", err)
 	}
-	var appConfig appconfig.AppConfig
-	err := copier.Copy(&appConfig, figConfig)
-	if err != nil {
-		log.Fatalf(fmt.Sprintf("Error creating config:%v", err))
-	}
-	return &appConfig
+	return (*appconfig.AppConfig)(&figConfig)
 }
